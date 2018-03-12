b6e30302e7c2d07115a01cf827377a1fa0a3fd63
jmerone <jmerone94@gmail.com>
2018-03-12 19:52:15 -0400

    Using the dplyr package

--- AnalyzingWisconsinBCancer.R
+++ AnalyzingWisconsinBCancer.R
@@ -1,3 +1,27 @@
 
-bcancer <- read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data",
+bcancer <- read.csv(
+  "http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data",
          header = FALSE)
+library(rpart)
+
+colnames(bcancer) <- c(
+'id_number',
+'Clump_Thickness',
+   'Uniformity_of_Cell_Size',
+   'Uniformity_of_Cell_Shape',
+   'Ma1rginal_Adhesion',
+ 'Single_Epithelial_Cell_Size',
+ 'Bare_Nuclei', 
+'Bland_Chromatin',
+'Normal_Nucleoli',
+'Mitoses', 
+'Class'
+)
+
+library(dplyr)
+
+bcancer <- bcancer %>% 
+  mutate(Class = ifelse(Class == 4, 1,0))
+
+rpart(Class ~ )
+View(bcancer)
